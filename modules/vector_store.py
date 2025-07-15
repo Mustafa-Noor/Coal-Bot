92a5ee1a62a77ad9ca0064719ef8a1736fa3e6d0
Mustafa Noor <144407367+Mustafa-Noor@users.noreply.github.com>
2025-07-15 17:32:17 +0500

    Update vector_store.py

--- modules/vector_store.py
+++ modules/vector_store.py
@@ -1,24 +1,20 @@
 import os
 from langchain.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from modules.doc_loader import load_pdf
+from modules.text_splitter import semantic_split
 
 CHROMA_PERSIST_DIR = "data/chroma_db"
 EMBEDDING_MODEL = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 def get_or_build_vector_store(docs):
-    if os.path.exists(CHROMA_PERSIST_DIR):
-        return Chroma(
-            persist_directory=CHROMA_PERSIST_DIR,
-            embedding_function=EMBEDDING_MODEL,
-        )
-    else:
-        db = Chroma.from_documents(
-            documents=docs,
-            embedding=EMBEDDING_MODEL,
-            persist_directory=CHROMA_PERSIST_DIR,
-        )
-        db.persist()
-        return db
+    db = Chroma.from_documents(
+        documents=docs,
+        embedding=EMBEDDING_MODEL,
+        persist_directory=CHROMA_PERSIST_DIR,
+    )
+    db.persist()
+    return db
 
 def load_chroma_vector_store():
     if os.path.exists(CHROMA_PERSIST_DIR) and os.listdir(CHROMA_PERSIST_DIR):
