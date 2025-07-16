26b3c48051061b7ec3b80526f6d8f919c7e651af
Mustafa Noor <144407367+Mustafa-Noor@users.noreply.github.com>
2025-07-16 10:37:50 +0500

    switched to faiss

--- modules/vector_store.py
+++ modules/vector_store.py
@@ -1,32 +1,30 @@
-mport os
-from langchain.vectorstores import Chroma
+import os
+import asyncio
+from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from modules.doc_loader import load_pdf
 from modules.text_splitter import semantic_split
-import asyncio
 
-
-CHROMA_PERSIST_DIR = "data/chroma_db"
+FAISS_PERSIST_DIR = "data/faiss_index"
 EMBEDDING_MODEL = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
+def save_faiss_index(db):
+    os.makedirs(FAISS_PERSIST_DIR, exist_ok=True)
+    db.save_local(FAISS_PERSIST_DIR)
+
+def load_faiss_index():
+    return FAISS.load_local(FAISS_PERSIST_DIR, EMBEDDING_MODEL)
+
 def get_or_build_vector_store(docs):
-    db = Chroma.from_documents(
-        documents=docs,
-        embedding=EMBEDDING_MODEL,
-        persist_directory=CHROMA_PERSIST_DIR,
-    )
-    db.persist()
+    db = FAISS.from_documents(docs, EMBEDDING_MODEL)
+    save_faiss_index(db)
     return db
 
-def load_chroma_vector_store():
-    if os.path.exists(CHROMA_PERSIST_DIR) and os.listdir(CHROMA_PERSIST_DIR):
-        return Chroma(
-            persist_directory=CHROMA_PERSIST_DIR,
-            embedding_function=EMBEDDING_MODEL
-        )
+def load_faiss_vector_store():
+    if os.path.exists(FAISS_PERSIST_DIR) and os.listdir(FAISS_PERSIST_DIR):
+        return load_faiss_index()
     else:
-        # 📄 Load + embed on the fly (for Streamlit Cloud)
         pdf_path = "doc/coal_book.pdf"
-        docs = asyncio.run(load_pdf(pdf_path))               # load from PDF
-        chunks = semantic_split(docs)            # split
-        return get_or_build_vector_store(chunks) # create + return
+        docs = asyncio.run(load_pdf(pdf_path))
+        chunks = semantic_split(docs)
+        return get_or_build_vector_store(chunks)
